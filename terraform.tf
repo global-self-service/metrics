8a09ba56faf77883d39f54801e1e88b10b04e8a4
Vinod Sai Kumar <vinodsai.kumar@global.com>
2021-01-13 17:03:44 +0000

    fix: upgraded to latest module version

--- terraform.tf
+++ terraform.tf
@@ -30,7 +30,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.eks-cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.eks-cluster.token
   }
 }
