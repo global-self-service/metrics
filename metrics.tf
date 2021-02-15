11e4bcaae656f68aeb308eab42e6f28f4dbc4a59
Stuart Clark <stuart.clark@global.com>
2021-02-15 15:28:03 +0000

    fix: Extra parameters

--- metrics.tf
+++ metrics.tf
@@ -19,6 +19,7 @@ module "metrics" {
   vpc_id                  = local.vpc_id
   zone_id                 = local.zone_id
   zone_name               = local.domain_name
+  prometheus_trusted_ips  = [module.common.devops_team.cidr, module.common.ct_team.cidr]
 
   external_labels = {
     environment = "selfservice-${local.environment}"
@@ -31,6 +32,7 @@ module "metrics" {
     auth_url       = local.oauth_authorization_endpoint
     token_url      = local.oauth_token_endpoint
     allowed_groups = [module.common.azure_groups["devops"]]
+    tenant_id      = module.common.azure_tenant_id
   }
 
   blackbox_target_urls = []
