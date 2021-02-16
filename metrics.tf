7bb4d6e312fda4264e19a2cbfe6633f9d2dd30e5
Stuart Clark <stuart.clark@global.com>
2021-02-16 11:40:17 +0000

    fix: Bump module

--- metrics.tf
+++ metrics.tf
@@ -1,5 +1,5 @@
 module "metrics" {
-  source = "github.com/global-devops-terraform/k8s-metrics?ref=v0.132.0"
+  source = "github.com/global-devops-terraform/k8s-metrics?ref=v0.132.1"
 
   read_access_groups = {
     "devaccess" = ["devaccess"]
