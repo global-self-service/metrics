b026b1f2bb73061a4614f1a13c8fb7c59d9d2223
James Hunsdon <61974479+JHunsGlobal@users.noreply.github.com>
2021-04-22 09:31:28 +0100

    feat: latest metrics module version v0.139.0
    Uping from k8s-metrics v0.138.3 to v0.139.0

--- metrics.tf
+++ metrics.tf
@@ -1,5 +1,5 @@
 module "metrics" {
-  source = "github.com/global-devops-terraform/k8s-metrics?ref=v0.138.3"
+  source = "github.com/global-devops-terraform/k8s-metrics?ref=v0.139.0"
 
   read_access_groups = {
     "devaccess" = ["devaccess"]
